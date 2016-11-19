fa7fe400fbabece13b199af7966de2a9e50fba7c
Louis <Louis.Antweiler@Gmail.com>
2016-11-19 03:45:51 -0700

    Filled in content for Work Experience. Made background images fixed. made headers fixed. Added a font for the headers. It's coming along nicely.

--- JS/WorkExperience.js
+++ JS/WorkExperience.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
+  $('ul').hide();
   $('h1').hover(function(){
     $(this).addClass('item_hover');
   },
   function(){
     $(this).removeClass('item_hover');
   });
+
+  $('h5').hover(function(){
+    $(this).addClass('h5hover');
+  },
+  function(){
+    $(this).removeClass('h5hover');
+  });
+
+  var arr=[0,1];
+
+  $('h5').click(function(){
+    var $cls = $(this).attr('class');
+    $('ul.'+$cls[0]).toggle();
+  });
+
+
 });
